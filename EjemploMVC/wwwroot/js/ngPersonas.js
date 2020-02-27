b87720351275e98c507b58ce3a952381cc46d68f
RubenRojasLinares <ruben.rojaslinares@outlook.com>
2020-02-27 00:26:47 -0600

    implementacion llamada por ajax

---
+++ EjemploMVC/wwwroot/js/ngPersonas.js
@@ -0,0 +1,18 @@
+﻿
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope, $http) {
+    $http({
+        url: "https://localhost:44323/api/Personas",
+        method: 'GET',
+        params: {
+            nombre: "mando nombre",
+            edad: 10,
+            mail: 'mandomail'
+        }
+    }).then(function (response) {
+        console.log("saved successfully");
+    }, function (response) {
+        console.log("Error message");
+    });
+});
